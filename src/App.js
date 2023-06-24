36030bed001d3d7e11cc6a5cca684291a1ff63a9
PK <nozumaz@gmail.com>
2023-06-24 15:57:45 -0400

    added NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Confirming this is working as expected</p>
+        <NavBar />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+        <p>NavBar test</p>
+      </div>
+  );
+}
+export default NavBar;
