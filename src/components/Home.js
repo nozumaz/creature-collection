edd4b96e031428716c5dfe5220f17edc6cccce27
PK <nozumaz@gmail.com>
2023-06-27 13:22:02 -0400

    Print db.json to console.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 
 function Home() {
+
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/toys")
+      .then((r) => r.json())
+      .then((toys) => console.log(toys));
+  }, []);
+
   return (
     <div>
         <h1>Home Page</h1>
