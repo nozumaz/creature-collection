c40c1ccfa5c8e8a6ad571da0c74e71ffa3156293
PK <nozumaz@gmail.com>
2023-06-30 14:37:31 -0400

    Added event listener to log 'submitted' when submit button is clicked.

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 
 function AddNew() {
+  
+  const [formData, setFormData] = useState({
+    name:"",
+    image:""
+  })
+
+  function handleSubmit() {
+    console.log("submitted");
+  }
+
   return (
-    <div>
-    <h1>AddNew2</h1>
-    <Form />
+    <div className="form-container">
+      <h1>AddNew2</h1>
+      <Form formData={formData} handleSubmit={handleSubmit} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form({ formData, handleSubmit }) {
+
+    function onSubmitClick(e) {
+        e.preventDefault();
+
+        handleSubmit();
+    }
+
     return (
         <>
-            <form className="form">
+            <form className="form" onSubmit={onSubmitClick}>
                 <input className="text-input" type="text" name="name" placeholder="name"></input>
                 <input className="text-input" type="text" name="image" placeholder="image URL"></input>
                 <input className="submit" value="submit" name="submit" type="submit"/>
