2721bc2b1cf1efa7f46f9cdbde9ccab0d57bd1ee
PK <nozumaz@gmail.com>
2023-06-30 14:26:00 -0400

    Added submit button to Form.js.

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,6 +6,7 @@ function Form() {
             <form className="form">
                 <input className="text-input" type="text" name="name" placeholder="name"></input>
                 <input className="text-input" type="text" name="image" placeholder="image URL"></input>
+                <input className="submit" value="submit" name="submit" type="submit"/>
             </form>
         </>
     )
