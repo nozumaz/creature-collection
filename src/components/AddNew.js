ee469582ca42d9919c867b83f628626e4c26822c
PK <nozumaz@gmail.com>
2023-07-01 12:20:03 -0400

    Checks for valid inputs in Form.js and adds empty string for notes field in AddNew.js

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -21,7 +21,8 @@ function AddNew() {
     })
     setFormData({
         name:"",
-        image:""
+        image:"",
+        notes:""
     })
   }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,28 @@ import React, { useState } from "react";
 
 function Form({ formData, handleChange, handleSubmit }) {
 
+    const [valid, setValid] = useState("")
+    const [error, setError] = useState("")
+
     function onSubmitClick(e) {
         e.preventDefault();
 
-        handleSubmit();
+        if (formData.name.trim() === "" ||
+            formData.image.trim() === "" ) {
+            setValid(false);
+            setError("Please complete both name and image fields.");
+            return;
+        } else if (
+            !formData.image.trim().includes(".jpeg" || ".jpeg")
+        ) {
+            setValid(false);
+            setError("Please enter a valid image URL.");
+            return;
+        } else {
+            setValid(true);
+            setError("");
+            handleSubmit();
+        }
     }
 
     return (
@@ -16,6 +34,7 @@ function Form({ formData, handleChange, handleSubmit }) {
                 <input className="text-input" type="text" onChange={handleChange} value={formData.notes} name="notes" placeholder="notes"></input>
                 <input className="submit" value="submit" name="submit" type="submit"/>
             </form>
+            <h2 className={valid ? "success" : "error"}>{valid ? "Success!" : error}</h2>
         </>
     )
 }
