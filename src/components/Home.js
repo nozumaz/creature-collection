bc716589ba5d8537f04e625be6ed079a45e5f55a
PK <nozumaz@gmail.com>
2023-07-01 08:20:15 -0400

    Implemented delete button to remove items from db.json

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,11 @@ function Home() {
 
   const [toys, setToys] = useState([]);
 
+  function handleDeleteClick(deleteID) {
+    const newList = toys.filter(toy => toy.id !== deleteID)
+    setToys(newList)
+  }
+
   useEffect(() => {
     fetch("http://localhost:4000/toys")
       .then((r) => r.json())
@@ -17,7 +22,7 @@ function Home() {
     <div>
         <h1>Home Page</h1>
         {toys.map((toy => (
-          <Item key={toy.id} toy={toy}/>
+          <Item key={toy.id} toy={toy} onDeleteClick={handleDeleteClick}/>
         )))}
     </div>
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function Item({toy}) {
+function Item({ toy, onDeleteClick }) {
+    function handleDeleteClick(id) {
+        fetch(`http://localhost:4000/toys/${id}`, {
+            method: "DELETE"
+       })
+    .then(resp => resp.json())
+    .then(() => onDeleteClick(id))
+    }
+
     return (
         <div>
             <h2>{toy.name}</h2>
             <img src={toy.image} alt="picture" width="300px"/>
+            <div className="button-container">
+                <button className="delete" onClick={() => handleDeleteClick(toy.id)}>Delete</button>
+            </div>
         </div>
     )
 }
