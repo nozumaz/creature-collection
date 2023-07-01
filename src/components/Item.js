d2ea3dc9cc2c1e9dbadcd242dcd7116e37c3e835
PK <nozumaz@gmail.com>
2023-07-01 07:59:53 -0400

    Tweaking home page/item appearance, added display of item name.

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,6 +3,7 @@ import React from "react";
 function Item({toy}) {
     return (
         <div>
+            <h2>{toy.name}</h2>
             <img src={toy.image} alt="picture" width="300px"/>
         </div>
     )
