3a3da4d08b5471f32f5b8298770c0aafbf3922c8
PK <nozumaz@gmail.com>
2023-07-01 16:06:45 -0400

    Updated css styling and minor capitalization changes.

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ function About() {
             <div className="info-box">
             <p>Small collection of mythical creatures that can be added and removed from.</p>
             <br/>
-            <h3>Github repository links:</h3>
-            <a href="https://github.com/nozumaz/flatiron-phase2-project">Frontend Repository</a>
+            <h3>Github Repository Links:</h3>
+            <a href="https://github.com/nozumaz/flatiron-phase2-project">Frontend React Repository</a>
             <br/>
             <br/>
             <a href="https://github.com/nozumaz/flatiron-phase2-project-backend">JSON Server Repository</a>
