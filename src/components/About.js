cbaa681406237b241a1d8b4d48c0eee789c02cd4
PK <nozumaz@gmail.com>
2023-07-02 10:23:10 -0400

    Changed description in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
         <div>
             <h1>About Page</h1>
             <div className="info-box">
-            <p>Small collection of mythical creatures that can be added and removed from.</p>
+            <p>Single page application made with create-react-app linked to a JSON server holding a collection of mythical creatures that can be added and removed from as desired.</p>
             <br/>
             <h3>Github Repository Links:</h3>
             <a href="https://github.com/nozumaz/flatiron-phase2-project">Frontend React Repository</a>
